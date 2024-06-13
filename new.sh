717e8c8202b64ed7ed398fa41c69eff247f2a3c6
VnzVPN <69085043+xhidrolix@users.noreply.github.com>
2024-06-13 16:56:33 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+
 apt install neofetch -y
 apt install fonts-powerline -y
 apt install gedit -y
 sleep 3
+cd /root
 rm -rf /root/.bashrc
 sleep 2
-wget "https://raw.githubusercontent.com/xhidrolix/bashrc/main/.bashrc"
-sleep 2
-chmod 644 /root/.bashrc
-echo "done"
+wget "https://raw.githubusercontent.com/xhidrolix/bashrc/main/bashrc.zip"
+unzip bashrc.zip ; rm -rf bashrc.zip
+chmood 644 .bashrc
