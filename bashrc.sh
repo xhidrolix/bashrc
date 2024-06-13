52812be374840b58c6e57f81cd366449e2963eab
VnzVPN <69085043+xhidrolix@users.noreply.github.com>
2024-06-13 16:39:00 +0700

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -6,7 +6,7 @@ apt install gedit -y
 sleep 3
 rm -rf /root/.bashrc
 sleep 2
-wget https://raw.githubusercontent.com/xhidrolix/bashrc/.bashrc
+wget https://raw.githubusercontent.com/xhidrolix/bashrc/main/.bashrc
 sleep 2
 chmod 644 /root/.bashrc
 echo "done"
